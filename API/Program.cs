b8bd0083a355467c15c54b26b739b5b3931f5204
RAVIMAURYA\Ravi <ravimaurya@outlook.com>
2024-03-06 22:15:55 +0530

    End of Section 2

---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+//Creates a WebApplicationBuilder object responsible for setting up the application's services and configuration.
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(option => 
+{
+    option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+//Below all are middleware
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
